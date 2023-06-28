8192eaf9836c2579de8b6fbef874c9f723dac582
maltsevalex <57605252+MasterSancho@users.noreply.github.com>
2023-06-28 14:31:16 +0300

    071-->Create Reviews - Backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -9,11 +9,15 @@ import {
  Button,
  Form,
 } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+import { toast } from 'react-toastify';
+import {
+ useGetProductDetailsQuery,
+ useCreateReviewMutation,
+} from '../slices/productsApiSlice';
 import { addToCart } from '../slices/cartSlice';
 
 const ProductScreen = () => {
@@ -23,16 +27,40 @@ const ProductScreen = () => {
  const navigate = useNavigate();
 
  const [qty, setQty] = useState(1);
+ const [rating, setRating] = useState(0);
+ const [comment, setComment] = useState('');
 
  const {
   data: product,
   isLoading,
+  refetch,
   error,
  } = useGetProductDetailsQuery(productId);
 
  const addToCartHandler = () => {
   dispatch(addToCart({ ...product, qty }));
   navigate('/cart');
+ };
+
+ const { userInfo } = useSelector((state) => state.auth);
+
+ const [createReview, { isLoading: loadingProductReview }] =
+  useCreateReviewMutation();
+
+ const submitHandler = async (e) => {
+  e.preventDefault();
+
+  try {
+   await createReview({
+    productId,
+    rating,
+    comment,
+   }).unwrap();
+   refetch();
+   toast.success('Review created successfully');
+  } catch (err) {
+   toast.error(err?.data?.message || err.error);
+  }
  };
 
  return (
