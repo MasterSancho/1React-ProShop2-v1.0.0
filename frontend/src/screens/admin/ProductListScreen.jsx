39eb65e60395a8c7382f5da15d57eea5db62d072
maltsevalex <57605252+MasterSancho@users.noreply.github.com>
2023-06-28 17:17:02 +0300

    074-->Paginate Component

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,9 +1,10 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaEdit, FaTrash } from 'react-icons/fa';
-import { useGetOrdersQuery } from '../../slices/ordersApiSlice';
+import { useParams } from 'react-router-dom';
 import Loader from '../../components/Loader';
 import Message from '../../components/Message';
+import Paginate from '../../components/Paginate';
 import { toast } from 'react-toastify';
 import {
  useGetProductsQuery,
@@ -12,7 +13,11 @@ import {
 } from '../../slices/productsApiSlice';
 
 const ProductListScreen = () => {
- const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+ const { pageNumber } = useParams();
+
+ const { data, isLoading, error, refetch } = useGetProductsQuery({
+  pageNumber,
+ });
 
  const [createProduct, { isLoading: createLoading, error: createError }] =
   useCreateProductMutation();
@@ -64,42 +69,45 @@ const ProductListScreen = () => {
    ) : error ? (
     <Message variant='danger'>{error}</Message>
    ) : (
-    <Table striped hover responsive className='table-sm'>
-     <thead>
-      <tr>
-       <th>ID</th>
-       <th>NAME</th>
-       <th>PRICE</th>
-       <th>CATEGORY</th>
-       <th>BRAND</th>
-       <th></th>
-      </tr>
-     </thead>
-     <tbody>
-      {products.map((product) => (
-       <tr key={product._id}>
-        <td>{product._id}</td>
-        <td>{product.name}</td>
-        <td>${product.price}</td>
-        <td>{product.category}</td>
-        <td>{product.brand}</td>
-        <td>
-         <LinkContainer to={`/admin/product/${product._id}/edit`}>
-          <Button variant='light' className='btn-sm mx-2'>
-           <FaEdit />
-          </Button>
-         </LinkContainer>
-         <Button
-          variant='danger'
-          className='btn-sm'
-          onClick={() => deleteHandler(product._id)}>
-          <FaTrash style={{ color: 'white' }} />
-         </Button>
-        </td>
+    <>
+     <Table striped hover responsive className='table-sm'>
+      <thead>
+       <tr>
+        <th>ID</th>
+        <th>NAME</th>
+        <th>PRICE</th>
+        <th>CATEGORY</th>
+        <th>BRAND</th>
+        <th></th>
        </tr>
-      ))}
-     </tbody>
-    </Table>
+      </thead>
+      <tbody>
+       {data.products.map((product) => (
+        <tr key={product._id}>
+         <td>{product._id}</td>
+         <td>{product.name}</td>
+         <td>${product.price}</td>
+         <td>{product.category}</td>
+         <td>{product.brand}</td>
+         <td>
+          <LinkContainer to={`/admin/product/${product._id}/edit`}>
+           <Button variant='light' className='btn-sm mx-2'>
+            <FaEdit />
+           </Button>
+          </LinkContainer>
+          <Button
+           variant='danger'
+           className='btn-sm'
+           onClick={() => deleteHandler(product._id)}>
+           <FaTrash style={{ color: 'white' }} />
+          </Button>
+         </td>
+        </tr>
+       ))}
+      </tbody>
+     </Table>
+     <Paginate pages={data.pages} page={data.page} isAdmin={true} />
+    </>
    )}
   </>
  );
