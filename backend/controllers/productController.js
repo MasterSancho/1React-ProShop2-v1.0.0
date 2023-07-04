e749038112f7802562307611b68d8ba0ece0cb9d
maltsevalex <57605252+MasterSancho@users.noreply.github.com>
2023-07-04 15:31:58 +0300

    080-->custom errors and checking for valid ObjectId

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -31,7 +31,7 @@ const getProductById = asyncHandler(async (req, res) => {
   return res.json(product);
  } else {
   res.status(404);
-  throw new Error('Resource not found');
+  throw new Error('Product not found');
  }
 });
 
